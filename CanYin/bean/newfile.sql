ca9d66c3d3bcc9b0906beb06821989108e97e174
xzhy527 <YiFeng@192.198.168.2>
2014-05-20 21:40:35 +0800

    YF_20_02

--- CanYin/bean/newfile.sql
+++ CanYin/bean/newfile.sql
@@ -17,4 +17,27 @@ CREATE TABLE db_user
 );
 
 
+CREATE TABLE db_table
+(
+	ID int NOT NULL IDENTITY ,
+	alias varchar(20),
+	tablename varchar(20),
+	type varchar(20),
+	position varchar(20),
+	limit float,
+	allow int,
+	state varchar(20),
+	isorder int,
+	SalesID int,
+	Water int,
+	prepay float,
+	starttime smalldatetime,
+	finishtime smalldatetime,
+	desrc varchar(200),
+	reserve varchar(100),
+	permission int,
+	PRIMARY KEY (ID)
+);
+
+
 
